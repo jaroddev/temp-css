a2018e675814afd97d48b63571c24e57349d6250
sitedevente <martinjarod17@gmail.com>
2019-12-25 15:12:33 +0100

    Can now test against CSRF

--- test/produit.php
+++ test/produit.php
@@ -2,10 +2,23 @@
 
 $page_name = 'produit.html.twig';
 
+$to_escape_value = '</h1><script>alert("haha")</script>';
+
+$product_name = 'Nom du produit';
+
+/**
+ * Change the value of options['name'] to test CSRF security failure and insert javascript into markup
+ * 
+ * $to_escape_value to test javascript code insertion
+ * $product_name to display a test product name 
+ */
+
+$test_value = $to_escape_value;
+
 $options = [
     'authentified' => false,
-    'name' => 'Nom du produit',
-    'image_defaut' => 'T-shirt_Adn.jpg'
+    'name' => $product_name,
+    'image_defaut' => 'T-shirt_Adn.jpg',
 ];
 
 require_once('test.php') ;
