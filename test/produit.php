994f34b2f12f7318f50b01021b86d9c9ee4b94eb
sitedevente <martinjarod17@gmail.com>
2019-12-25 05:07:28 +0100

    Added values to twig template and as options in the test

--- test/produit.php
+++ test/produit.php
@@ -4,6 +4,8 @@ $page_name = 'produit.html.twig';
 
 $options = [
     'authentified' => false,
+    'name' => 'Nom du produit',
+    'image_defaut' => 'T-shirt_Adn.jpg'
 ];
 
 require_once('test.php') ;
