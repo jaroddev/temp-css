90047df10b696d4c9723b57df44163677ffd39b3
sitedevente <martinjarod17@gmail.com>
2020-01-09 17:39:44 +0100

    Generate dynamically association input and label in product/show

--- test/produit.php
+++ test/produit.php
@@ -21,6 +21,59 @@ $product_array = [
     ]
 ];
 
+$charity_array = [
+    [
+        'id' => 0,
+        'name' => 'WWF',
+        'image_name' => 'logo0.png',
+    ],
+    [
+        'id' => 1,
+        'name' => 'Les restaurants du cœur',
+        'image_name' => 'logo1.png',
+    ],
+    [
+        'id' => 2,
+        'name' => 'Secours populaire français',
+        'image_name' => 'logo2.png',
+    ],
+    [
+        'id' => 3,
+        'name' => 'UNICEF',
+        'image_name' => 'logo3.png',
+    ],
+    [
+        'id' => 4,
+        'name' => 'Action contre La Faim',
+        'image_name' => 'logo4.png',
+    ],
+    [
+        'id' => 5,
+        'name' => 'Sea Shepherd Conservation Society',
+        'image_name' => 'logo5.png',
+    ],
+    [
+        'id' => 6,
+        'name' => 'Croix-Rouge française',
+        'image_name' => 'logo6.png',
+    ],
+    [
+        'id' => 7,
+        'name' => 'Médecins sans frontières',
+        'image_name' => 'logo7.png',
+    ],
+    [
+        'id' => 8,
+        'name' => '30 Millions d\'Amis',
+        'image_name' => 'logo8.png',
+    ],
+    [
+        'id' => 9,
+        'name' => 'Institut Pasteur',
+        'image_name' => 'logo9.png',
+    ],
+];
+
 /**
  * Change the value of options['name'] to test CSRF security failure and insert javascript into markup
  * 
@@ -32,6 +85,7 @@ $options = [
     'authentified' => false,
     'name' => $product_name,
     'product_array' => $product_array,
+    'charity_array' => $charity_array,
 ];
 
 require_once('test.php') ;
